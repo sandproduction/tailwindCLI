36ed58b20b60489c1079b1edf984485c440a0307
sandproduction <sandproductions77@gmail.com>
2024-07-24 18:47:05 +0700

    pembaruan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/**/*.{html,js}'],
-  theme: {
-    extend: {},
-  },
+  darkMode:'class',
+            theme:{
+                extend:{
+                    spacing:{
+                        13: '3.25rem',
+                    },
+                    fontFamily:{
+                        poppins:['Poppins']
+                    },
+                    colors:{
+                      matcha:'#88D66C',
+                      greenSand:'#40534C',
+                    },
+                    animation:{
+                        'spin-slow': 'spin 3s linear',
+                        'goyang': 'goyang 1s ease-in-out infinite'
+                    },
+                    keyframes:{
+                        goyang:{
+                            '0%, 100%':{ transform: 'skew(-5deg)'},
+                            '50%': {transform: 'skew(5deg)'}
+                        }
+                    }
+                }
+            },
   plugins: [],
 }
 
